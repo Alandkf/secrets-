e51a078c9182577ba0ed9f9f3dbe66c6d4f2eba3
AlandKf <aland.zland@gmail.com>
2024-03-20 23:05:34 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('view engine', 'ejs')
+  .set('views', path.join(__dirname, 'views'));
+
+
+
+mongoose.connect("mongodb://localhost:27017/userDB")
+.then(()=>{console.log("mongoose connected")})
+.catch((err)=>{console.log("could not connect to mongoose"+err)});
+
+const userSchema = mongoose.Schema({
+    username: String,
+    password: String
+});
+
+
+const secret = "theSecretBetweenUsIsDead";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+const User = mongoose.model("user", userSchema);
+
+app.route('/')
+.get((req,res)=>{
+    res.render('home');
+})
+
+app.route('/login')
+.get((req,res)=>{
+    res.render('login');
+})
+.post((req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({username: username, password: password})
+  .then((user)=>{
+      if (!user) {
+          return res.send('Invalid Username or Password').status(401).end()
+      } else {
+        res.send('Username: ' + user.username)
+      }
+  })
+  .catch((err)=>console.log(err));
+})
+
+app.route('/register')
+.get((req,res)=>{
+    res.render('register');
+})
+.post((req,res)=>{
+    const newUser = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    newUser.save()
+    .then((user)=>{
+        console.log(user)
+        res.render('secrets')})
+    .catch((error)=> {console.log(error);})
+    })
+
+app.route('/secrets')
+    .get((req,res)=>{
+        res.render('secrets');
+    })
+
+app.listen(3000,(req,res)=>{
+    console.log("server is running on port 3000");
+})
