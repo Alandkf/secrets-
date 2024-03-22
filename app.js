5a22541655d50f541c279a48157d29eb7330fafe
AlandKf <aland.zland@gmail.com>
2024-03-23 01:49:01 +0300

    I guess level 5 ends here but still need more improvemeny

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const saltRounds = 10;
 // npm i passport passport-local passport-local-mongoose express-session
 const session = require('express-session');
 const passport = require('passport');//making session private
-const LocalStrategy = require('passport-local').Strategy;
+// const LocalStrategy = require('passport-local').Strategy; // we don't need it
 const passportLocalMongoose = require('passport-local-mongoose');//making session private
 
 const app = express();
@@ -29,22 +29,53 @@ app
   .set('view engine', 'ejs')
   .set('views', path.join(__dirname, 'views'));
 
+  app.use(session({
+      secret: process.env.SECRETS,
+      resave: false,
+      saveUninitialized: false
+      // cookie: { secure: true }
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
 
 
 mongoose.connect("mongodb://localhost:27017/userDB")
 .then(()=>{console.log("mongoose connected")})
 .catch((err)=>{console.log("could not connect to mongoose"+err)});
+// mongoose.set("useCreateIndex",true) // we don't need this anymore lvl 5
 
 const userSchema = mongoose.Schema({
     username: String,
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose)
+
 // encrypt level 1:
 // const secret = process.env.SECRETS
 // userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 const User = mongoose.model("user", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(
+    (id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+        .catch(err => {
+            done(err, null);
+        });
+});
+
+
 
 app.route('/')
 .get((req,res)=>{
@@ -57,6 +88,24 @@ app.route('/login')
 })
 .post((req,res)=>{
 
+    const user = new User({
+        username : req.body.username,
+        password : req.body.password
+    });
+
+    req.login(user, (err)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            passport.authenticate('local')(req, res , 
+            (req,ress)=>{
+                res.redirect('secrets');
+            });
+
+        }
+    })
+
 })
 
 app.route('/register')
@@ -64,9 +113,43 @@ app.route('/register')
 .get((req,res)=>{
     res.render('register');
 })
-.post((req,res)=>{
 
-    })
+
+.post( (req, res) => {
+
+      User.register({ username: req.body.username }, req.body.password)
+        .then((user) => {
+          passport.authenticate("local")(req, res, () => {
+            res.redirect("secrets");
+          });
+        })
+        .catch((err) => {
+          console.log("Error: " + err);
+          res.redirect("/register");
+        });
+  });
+      app.get("/secrets", (req, res) => {
+        if (req.isAuthenticated()) {
+          res.render("secrets");
+        } else {
+          res.redirect("/login");
+        }
+      });
+
+
+      app.get("/logout", (req, res) => {
+        req.logout((err) => {
+            if (err) {
+                console.error("Error logging out:", err);
+                return res.redirect("/"); // Redirect even if there's an error
+            }
+            res.redirect("/");
+        });
+    });
+    
+
+      
+
 
 app.listen(3000,(req,res)=>{
     console.log("server is running on port 3000");
